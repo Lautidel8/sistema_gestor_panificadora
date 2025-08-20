fad20cf8f5ae784757b55c7e4d87755217199140
Lauti <lautieberhardt8@gmail.com>
2025-08-20 14:44:27 -0300

    eliminacion de archivos

--- frontend/pantalla_principal.py
+++ frontend/pantalla_principal.py
@@ -11,7 +11,7 @@ class vista_principal:
     def config_page(self):
         self.page.controls.clear()
         self.page.title = "Pantalla Principal"
-        self.page.bgcolor = "#040404"
+        self.page.bgcolor = "#ffebdf"
         self.page.scroll = ft.ScrollMode.AUTO
         self.conexion = conectar()
         self.cursor = self.conexion.cursor()
@@ -27,65 +27,98 @@ class vista_principal:
             filas.append(
                 ft.DataRow(
                     cells=[
-                        ft.DataCell(ft.Text(str(pedido[1]))),
-                        ft.DataCell(ft.Text(str(pedido[5]))),
-                        ft.DataCell(ft.Text(str(pedido[3]))), 
-                        ft.DataCell(ft.Text(str(pedido[4]))),
-                        ft.DataCell(ft.Text(str(pedido[6]))),
-                        ft.DataCell(ft.Text(str(pedido[2]))),
+                        ft.DataCell(ft.Text(str(pedido[1]), style=ft.TextStyle(color="#37373A"))),
+                        ft.DataCell(ft.Text(str(pedido[5]), style=ft.TextStyle(color="#37373A"))),
+                        ft.DataCell(ft.Text(str(pedido[3]), style=ft.TextStyle(color="#37373A"))), 
+                        ft.DataCell(ft.Text(str(pedido[4]), style=ft.TextStyle(color="#37373A"))),
+                        ft.DataCell(ft.Text(str(pedido[6]), style=ft.TextStyle(color="#37373A"))),
+                        ft.DataCell(ft.Text(str(pedido[2]), style=ft.TextStyle(color="#37373A"))),
                     ]
                 )
             )
 
 
-    
         grilla_pedidos = ft.Container(
             expand=True,
+            bgcolor="#fdd0b5",
             content=ft.Column(
                 [
                     ft.DataTable(
                         columns=[
-                            ft.DataColumn(ft.Text("Nombre pedido")),
-                            ft.DataColumn(ft.Text("Cantidad")),
-                            ft.DataColumn(ft.Text("Precio")),
-                            ft.DataColumn(ft.Text("Cliente")),
-                            ft.DataColumn(ft.Text("Fecha del pedido")),
-                            ft.DataColumn(ft.Text("Estado del pedido")),
+                            ft.DataColumn(ft.Text("Nombre pedido", style=ft.TextStyle(color="#37373A"))),
+                            ft.DataColumn(ft.Text("Cantidad", style=ft.TextStyle(color="#37373A"))),
+                            ft.DataColumn(ft.Text("Precio", style=ft.TextStyle(color="#37373A"))),
+                            ft.DataColumn(ft.Text("Cliente", style=ft.TextStyle(color="#37373A"))),
+                            ft.DataColumn(ft.Text("Fecha del pedido", style=ft.TextStyle(color="#37373A"))),
+                            ft.DataColumn(ft.Text("Estado del pedido", style=ft.TextStyle(color="#37373A"))),
                         ],
                         rows=filas
                     )
                 ],
                 scroll=ft.ScrollMode.ALWAYS,  # Scroll en el Column
             ),
-            height=600,
+            height=550,
+            width=1100,
             border_radius=10,
             padding=10,
         )
 
-
-        boton_superior = ft.Container(
-            content=ft.ElevatedButton("Cargar nuevo pedido"),
-            padding=10
+        barra_navegacion = ft.Container(
+            border_radius=10,
+            bgcolor="#fdd0b5",
+            padding=10,
+            height=600,
+            width=120,
+            expand=True,
+            content=ft.Column(
+                controls=[
+                    ft.Icon("home"),
+                    ft.Icon("location_on"),
+                    ft.Icon("calendar_month"),
+                    ft.Icon("settings"),
+                    ft.Icon("power_settings_new"),
+                ],
+                alignment=ft.MainAxisAlignment.START,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                spacing=20,
+                expand=True
+            ),
+        )
+        
+        texto_principal = ft.Text(
+            "Pedidos",
+            color="#37373A",
+            size=30
+        )
+        
+        boton_modificar_pedido = ft.ElevatedButton(
+            "x",
+            #on_click=self.validar_ingreso,
+            color="#fdd0b5",
+            bgcolor="#37373A",
+            width=100,
+        )
+        
+        boton_cargar_pedido = ft.ElevatedButton(
+            "y",
+            #on_click=self.validar_ingreso,
+            color="#fdd0b5",
+            bgcolor="#37373A",
+            width=100,
         )
 
         self.page.add(
             ft.Row(
                 expand=True,
                 controls=[
-                ft.Column(
-                    controls=[
-                        boton_superior,
-                    ],
-                ),
-                ft.Column(
-                    expand=True,
-                    controls=[
-                        grilla_pedidos
-                    ],
-                )]
-            )    
+                    ft.Column(
+                        expand=False,
+                        controls=[barra_navegacion],
+                    ),
+                    ft.Column(
+                        expand=True,
+                        controls=[texto_principal,grilla_pedidos],
+                    ),
+                ]
+            )
         )
-
-                    # alignment=ft.MainAxisAlignment.CENTER,
-                    # horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                    # spacing=30
--- frontend/prueba.py
+++ frontend/prueba.py
@@ -1,90 +1,225 @@
-import flet as ft
-from backend.controladores_pana.control_Contrasenia import Valida_contrasenia
-from frontend.pantalla_principal import VistaPrincipal  # Ahora importamos la versión en clase
 
-class PantallaLogin:
-    def __init__(self, page: ft.Page):
+# Magno Efren 
+# https://www.youtube.com/@MagnoEfren/videos
+
+import flet as ft 
+import random 
+import time 
+import threading 
+
+
+class MyApp(ft.Control):
+    def __init__(self, page):
+        super().__init__(expand=True)
         self.page = page
-        self.setup_page()
-        self.armar_vista()
+        self.x = 0
 
-    def setup_page(self):
-        """Configuración inicial de la pantalla."""
-        self.page.controls.clear()
-        self.page.title = "Sistema Panadería Janssen"
-        self.page.bgcolor = "#ffebdf"
+        self.slider1 = ft.Slider( value= 30, min = 0, max = 100,
+                                 inactive_color="black", expand= True,
+                                 active_color= "white",
+                                 on_change= self.value_slider1)
 
-    def validar_ingreso(self, e):
-        """Valida la contraseña ingresada."""
-        resultado = Valida_contrasenia.verficar_contra(self.contra.value)
-        if resultado:
-            self.page.go("/pantalla_principal")
-        else:
-            print("Contraseña incorrecta")
-        self.page.update()
+        self.slider2 = ft.Slider( value= 50, min = 0, max = 100,
+                                 inactive_color="black", expand= True,
+                                 active_color= "white",
+                                 on_change= self.value_slider2)
+        
+        self.slider3 = ft.Slider( value= 20, min = 0, max = 100,
+                                 inactive_color="black", expand= True,
+                                 active_color= "white",
+                                 on_change= self.value_slider3)
+        
+        self.slider4 = ft.Slider( value= 80, min = 0, max = 100,
+                                 inactive_color="black", expand= True,
+                                 active_color= "white",
+                                 on_change= self.value_slider4)
+        
 
-    def armar_vista(self):
-        """Arma la interfaz del login."""
-        texto_titulo = ft.Text(
-            "Sistema de Panadería Janssen",
-            color="#fdd0b5",
-            size=80
-        )
-        texto1 = ft.Text(
-            "Ingrese la contraseña para continuar:",
-            color="#37373A",
-            size=15
-        )
+        self.indicator1 = ft.Text(value= "30%", color= "black")
+        self.indicator2 = ft.Text(value= "50%", color= "black")
+        self.indicator3 = ft.Text(value= "20%", color= "black")
+        self.indicator4 = ft.Text(value= "80%", color= "black")    
 
-        self.contra = ft.TextField(
-            label="Contraseña",
-            can_reveal_password=True,
-            password=True,
-            border_color="#fdd0b5",
-            focused_border_color="#fdd0b5",
-            label_style=ft.TextStyle(color="#37373A"),
-            text_style=ft.TextStyle(color="#37373A"),
-            width=400
-        )
 
-        boton_ingresar = ft.ElevatedButton(
-            "Ingresar",
-            on_click=self.validar_ingreso,
-            color="#fdd0b5",
-            bgcolor="#37373A",
-            width=200,
-        )
 
-        self.page.add(
-            ft.Container(
-                content=ft.Column(
-                    [
-                        texto_titulo,
-                        texto1,
-                        self.contra,
-                        boton_ingresar
-                    ],
-                    alignment=ft.MainAxisAlignment.CENTER,
-                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                    spacing=30
+        self.control1 = ft.Row([self.slider1, self.indicator1], spacing= 0, expand=True)
+        self.control2 = ft.Row([self.slider2, self.indicator2], spacing= 0, expand=True)
+        self.control3 = ft.Row([self.slider3, self.indicator3], spacing= 0, expand=True)
+        self.control4 = ft.Row([self.slider4, self.indicator4], spacing= 0, expand=True)
+
+
+
+        self.data1 = [ft.LineChartData(data_points=[],
+                                       curved=True,
+                                       stroke_width=2,
+                                       color  = "black",
+                                       point= True,
+                                       )]
+        
+
+        self.graph1 = ft.LineChart(data_series= self.data1,
+                                   min_x= 0,
+                                   max_x= 50,
+                                   max_y= 50,
+                                   min_y= 0,
+                                   point_line_start=0,
+                                   expand=True,
+                                   interactive= False,
+                                   left_axis= ft.ChartAxis(visible= True,
+                                                           labels_size= 30,),
+
+                                   bottom_axis= ft.ChartAxis(visible= True,
+                                                           labels_size= 30,),
+                                   border= ft.Border(
+                                       bottom= ft.BorderSide(2,
+                                                             ft.colors.with_opacity(0.3,
+                                                                                    "white")),
+                                       left= ft.BorderSide(2,
+                                                             ft.colors.with_opacity(0.3,
+                                                                                    "white")),
+                                   ),
+                                   )
+        
+
+
+        self.data2 = [ ft.LineChartData(data_points= [ft.LineChartDataPoint(0, int(self.slider1.value)),
+                                                      ft.LineChartDataPoint(25, int(self.slider2.value)),
+                                                      ft.LineChartDataPoint(50, int(self.slider3.value)),
+                                                      ft.LineChartDataPoint(100, int(self.slider4.value)),
+                                                      ],
+                                        curved= True,
+                                        stroke_width=2,
+                                        color = "black",
+                                        point = True,
+                                        below_line_gradient= ft.LinearGradient(["white", "purple"]) 
+                                                      )]
+
+
+
+
+
+        self.graph2 = ft.LineChart( data_series= self.data2,
+                                   min_x=0,
+                                   max_x= 100,
+                                   min_y=0,
+                                   max_y= 100,
+                                   point_line_start=0,
+                                   expand=True,
+                                   interactive= False,
+                                   border= ft.Border(
+                                       bottom= ft.BorderSide(2,
+                                                             ft.colors.with_opacity(0.3,
+                                                                                    "white")),
+                                       left= ft.BorderSide(2,
+                                                             ft.colors.with_opacity(0.3,
+                                                                                    "white")),
+
+                                   ),
+                                   )
+
+
+        self.threading = threading.Thread( target= self.real_time_data)
+        self.threading.start()
+
+
+        self.content = ft.Column(
+            expand=True,
+            controls=[
+                ft.Container(
+                    expand=True,
+                    gradient= ft.LinearGradient(["purple", "white"], rotation=30),
+                    border_radius=10,
+                    content= ft.Column(
+                        alignment= ft.MainAxisAlignment.CENTER,
+                        controls=[
+                            ft.Container(
+                                height=60,
+                                padding=10,
+                                alignment= ft.alignment.center,
+                                content= ft.Text("Gráfica en tiempo real",
+                                                 color = "black",
+                                                 font_family="vivaldi",
+                                                 size=30,
+                                                 weight="bold")
+                            ),
+                        
+                        ft.Container(
+                            expand=True,
+                            content= self.graph1,
+                            padding= 20,
+                        )
+                        ]
+                    )
                 ),
-                alignment=ft.alignment.center,
-                expand=True
-            )
-        )
 
+
+                ft.Container(
+                    gradient= ft.LinearGradient(["purple", "white"]),
+                    expand=True,
+                    border_radius= 10,
+                    padding=10,
+                    content= ft.Row(
+                        spacing=20,
+                        controls=[
+                            ft.Column(
+                                expand=True,
+                                controls= [
+                                    self.control1,
+                                    self.control2,
+                                    self.control3,
+                                    self.control4,
+                                ]
+                            ),
+
+                            ft.Column(
+                                expand=True,
+                                controls=[
+                                    self.graph2
+                                ]
+                            )
+                        ]
+                    )
+                ),
+            ]
+        )
+    def build(self):
+        return self.content 
+    
+
+    def value_slider1(self, e):
+        self.indicator1.value = str(f"{int(self.slider1.value)} %")
+        self.data2[0].data_points[0].y = self.slider1.value
+        self.update()
+        
+    def value_slider2(self, e):
+        self.indicator2.value = str(f"{int(self.slider2.value)} %")
+        self.data2[0].data_points[1].y = self.slider2.value
+        self.update()
+        
+    def value_slider3(self, e):
+        self.indicator3.value = str(f"{int(self.slider3.value)} %")
+        self.data2[0].data_points[2].y = self.slider3.value
+        self.update()
+
+    def value_slider4(self, e):
+        self.indicator4.value = str(f"{int(self.slider4.value)} %")
+        self.data2[0].data_points[3].y = self.slider4.value
+        self.update()
+
+
+    def real_time_data(self):
+        while True:
+            self.x  += 1
+            data = random.randint(5, 45)
+            self.data1[0].data_points.append(ft.LineChartDataPoint(self.x, data))
+
+            if len(self.data1[0].data_points) ==50:
+                self.x = 0
+                self.data1[0].data_points.clear()
+            self.update()
+            time.sleep(0.3)
 
 def main(page: ft.Page):
-    """Gestión de rutas y vistas."""
-    def cambiar_pantalla(e):
-        if page.route == "/":
-            PantallaLogin(page)
-        elif page.route == "/pantalla_principal":
-            VistaPrincipal(page)
-        page.update()
-
-    page.on_route_change = cambiar_pantalla
-    page.go(page.route)
-
+    page.add(MyApp(page))
 
 ft.app(target=main)
